699297e024f337321cca544ea32070f591b089ca
Gabriel Gama <gbr.gama99@gmail.com>
2022-04-11 22:23:38 -0300

    Criação da Classe ApostaOut, usada na classe ApostaController

---
+++ src/main/java/com/api/bet/model/dto/ApostaOut.java
@@ -0,0 +1,14 @@
+package com.api.bet.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ApostaOut {
+
+    private String numeroAposta;
+    private String nomeApostador;
+    private String emailApostadoor;
+
+}
