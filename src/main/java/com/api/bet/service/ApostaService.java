cb70658466bc247a7ba5335bf6c70b68e566b815
Gabriel Gama <gbr.gama99@gmail.com>
2022-04-11 23:23:31 -0300

    Tratamento de Exceptions

--- src/main/java/com/api/bet/service/ApostaService.java
+++ src/main/java/com/api/bet/service/ApostaService.java
@@ -7,8 +7,13 @@ import com.api.bet.model.dto.ApostaOut;
 import com.api.bet.repository.ApostaRepository;
 import com.api.bet.repository.ApostadorRepository;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.constraints.NotBlank;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -19,13 +24,21 @@ public class ApostaService {
     private final ApostadorRepository apostadorRepository;
     private final ApostaRepository apostaRepository;
 
+    ApostaOut apostaOut = null;
 
     public ApostaOut salvaAposta(ApostaIn apostaIn){
-        Optional<Apostador> apostador = apostadorRepository.findById(apostaIn.getIdApostador());
-        String numAposta = UUID.randomUUID().toString();
-        Aposta aposta = new Aposta(numAposta, apostador.get());
 
-        apostaRepository.save(aposta);
-        return new ApostaOut(numAposta, apostador.get().getName(), apostador.get().getEmail());
+        try{
+            Optional<Apostador> apostador = apostadorRepository.findById(apostaIn.getIdApostador());
+            String numAposta = UUID.randomUUID().toString();
+            Aposta aposta = new Aposta(numAposta, apostador.get());
+
+            apostaRepository.save(aposta);
+            apostaOut = new ApostaOut(numAposta, apostador.get().getName(), apostador.get().getEmail());
+        }catch (NoSuchElementException exception){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        }
+        return apostaOut;
     }
 }
