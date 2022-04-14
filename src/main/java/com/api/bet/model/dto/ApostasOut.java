ff75378a0834a509d4c564bd1e1f7d1e77d42d8f
Gabriel Gama <gbr.gama99@gmail.com>
2022-04-14 00:27:20 -0300

    Implementação da classe ApostasOut

---
+++ src/main/java/com/api/bet/model/dto/ApostasOut.java
@@ -0,0 +1,17 @@
+package com.api.bet.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApostasOut {
+
+    private String numeroAposta;
+    private Long apostador;
+
+}
