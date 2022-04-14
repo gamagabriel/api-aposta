e633e5dff2048e93b65a5ad85c624aad090ccb00
Gabriel Gama <gbr.gama99@gmail.com>
2022-04-14 00:26:48 -0300

    @GetMapping para leitura de todas as apostas e busca por ID

--- src/main/java/com/api/bet/controller/ApostaController.java
+++ src/main/java/com/api/bet/controller/ApostaController.java
@@ -1,15 +1,16 @@
 package com.api.bet.controller;
 
+import com.api.bet.model.dto.Aposta;
 import com.api.bet.model.dto.ApostaIn;
 import com.api.bet.model.dto.ApostaOut;
+import com.api.bet.model.dto.ApostasOut;
 import com.api.bet.service.ApostaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 import static org.springframework.http.HttpStatus.CREATED;
 
@@ -20,9 +21,19 @@ public class ApostaController {
     private final ApostaService apostaService;
 
     @PostMapping("/aposta")
-    public ResponseEntity <ApostaOut> salvaAposta(@Valid @RequestBody ApostaIn apostaIn){
+    public ResponseEntity<ApostaOut> salvaAposta(@Valid @RequestBody ApostaIn apostaIn) {
         ApostaOut apostaOut = apostaService.salvaAposta(apostaIn);
 
         return ResponseEntity.status(CREATED).body(apostaOut);
+    }
+
+    @GetMapping("/read-all-apostas")
+    public List<Aposta> findAll() {
+        return apostaService.findAll();
+    }
+
+    @GetMapping("/getaposta/{id}")
+    public List <ApostasOut> apostaById(@PathVariable Long id) {
+        return apostaService.apostaById(id);
     }
 }
