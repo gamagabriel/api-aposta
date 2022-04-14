8f9c63a53868539fae7c6b9a6388fc39f46cf73c
Gabriel Gama <gbr.gama99@gmail.com>
2022-04-14 00:38:55 -0300

    Refatoração

--- src/main/java/com/api/bet/model/dto/ApostaOut.java
+++ src/main/java/com/api/bet/model/dto/ApostaOut.java
@@ -2,9 +2,11 @@ package com.api.bet.model.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Getter
+@NoArgsConstructor
 public class ApostaOut {
 
     private String numeroAposta;
--- src/main/java/com/api/bet/repository/ApostaRepository.java
+++ src/main/java/com/api/bet/repository/ApostaRepository.java
@@ -2,8 +2,14 @@ package com.api.bet.repository;
 
 import com.api.bet.model.dto.Aposta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ApostaRepository extends JpaRepository<Aposta, String > {
+public interface ApostaRepository extends JpaRepository<Aposta, Long> {
+    @Query(value = "SELECT * FROM Aposta WHERE id_apostador = ?", nativeQuery = true)
+    List<Aposta> findByApostadorId(@Param("idApostador") Long id);
 }
