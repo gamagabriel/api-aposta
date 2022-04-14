8f8ebe1785916a7d3b23856ea2df23c9019ff221
Gabriel Gama <gbr.gama99@gmail.com>
2022-04-14 00:39:38 -0300

    Refatoração

--- src/main/java/com/api/bet/service/ApostadorService.java
+++ src/main/java/com/api/bet/service/ApostadorService.java
@@ -5,7 +5,11 @@ import com.api.bet.model.dto.ApostadorIn;
 import com.api.bet.repository.ApostadorRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.webjars.NotFoundException;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -17,5 +21,17 @@ public class ApostadorService {
     public void salvaApostador(ApostadorIn apostadorIn){
         Apostador apostador = modelMapper.map(apostadorIn, Apostador.class);
         apostadorRepository.save(apostador);
+    }
+
+    public void delete(Long id){
+        try{
+            apostadorRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+            throw new NotFoundException(e.getMessage());
+        }
+    }
+
+    public List<Apostador> findAll() {
+        return apostadorRepository.findAll();
     }
 }
