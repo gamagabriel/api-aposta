8c152e817ab0f0d35b0fa6d6e498fd98465cd469
Gabriel Gama <gbr.gama99@gmail.com>
2022-04-14 00:29:31 -0300

    Implementação da classe Service para CRUD Get

--- src/main/java/com/api/bet/service/ApostaService.java
+++ src/main/java/com/api/bet/service/ApostaService.java
@@ -4,18 +4,15 @@ import com.api.bet.model.Apostador;
 import com.api.bet.model.dto.Aposta;
 import com.api.bet.model.dto.ApostaIn;
 import com.api.bet.model.dto.ApostaOut;
+import com.api.bet.model.dto.ApostasOut;
 import com.api.bet.repository.ApostaRepository;
 import com.api.bet.repository.ApostadorRepository;
 import lombok.RequiredArgsConstructor;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import javax.validation.constraints.NotBlank;
-import java.util.NoSuchElementException;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 @RequiredArgsConstructor
 @Service
@@ -24,21 +21,39 @@ public class ApostaService {
     private final ApostadorRepository apostadorRepository;
     private final ApostaRepository apostaRepository;
 
+
     ApostaOut apostaOut = null;
 
-    public ApostaOut salvaAposta(ApostaIn apostaIn){
+    public ApostaOut salvaAposta(ApostaIn apostaIn) {
 
-        try{
+        try {
             Optional<Apostador> apostador = apostadorRepository.findById(apostaIn.getIdApostador());
             String numAposta = UUID.randomUUID().toString();
             Aposta aposta = new Aposta(numAposta, apostador.get());
 
             apostaRepository.save(aposta);
             apostaOut = new ApostaOut(numAposta, apostador.get().getName(), apostador.get().getEmail());
-        }catch (NoSuchElementException exception){
+
+        } catch (NoSuchElementException exception) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 
         }
         return apostaOut;
+    }
+
+    public List<Aposta> findAll() {
+        return apostaRepository.findAll();
+    }
+
+    public List<ApostasOut> apostaById(Long id) {
+        List<Aposta> aposta = apostaRepository.findByApostadorId(id);
+        List<ApostasOut> list = new ArrayList<>();
+        aposta.forEach(aposta1 -> {
+            ApostasOut out = new ApostasOut();
+            out.setApostador(aposta1.getApostador().getId());
+            out.setNumeroAposta(aposta1.getNumeroAposta());
+            list.add(out);
+        });
+        return list;
     }
 }
